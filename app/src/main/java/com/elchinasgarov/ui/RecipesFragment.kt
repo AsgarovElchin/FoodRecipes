b643f2d67578f12fac6870051e3f143eeee6cf0d
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-07-06 14:41:43 +0400

    observing data from viewModel done

--- app/src/main/java/com/elchinasgarov/ui/RecipesFragment.kt
+++ app/src/main/java/com/elchinasgarov/ui/RecipesFragment.kt
@@ -1,7 +1,6 @@
 package com.elchinasgarov.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.MainViewModel
+import com.adapters.RecipesAdapter
 import com.elchinasgarov.foodrecipes.R
 import com.util.Constants.API_KEY
 import com.util.NetworkResult
@@ -18,15 +18,62 @@ import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+    private lateinit var mainViewModel: MainViewModel
     private lateinit var mView: View
+    private val mAdapter by lazy { RecipesAdapter() }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         mView = inflater.inflate(R.layout.fragment_recipes, container, false)
+        mainViewModel = ViewModelProvider(requireActivity()).get((MainViewModel::class.java))
+        setUpRv()
+        requestApiData()
         return mView
     }
 
 
+    private fun requestApiData() {
+        mainViewModel.getRecipes(applyQuesries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    response.data?.let { mAdapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    Toast.makeText(requireContext(), "Loading", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+        }
+
+    }
+
+    private fun applyQuesries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries["number"] = "50"
+        queries["apiKey"] = API_KEY
+        queries["type"] = "snack"
+        queries["diet"] = "vegan"
+        queries["addRecipeInformation"] = "true"
+        queries["fillIngredients"] = "true"
+
+        return queries
+
+    }
+
+
+    private fun setUpRv() {
+        mView.recyclerView.adapter = mAdapter
+        mView.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
