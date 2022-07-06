6a7c74f0f900e84e7d0ab61839228c34631d9fcc
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-07-06 16:34:58 +0400

    dao interface done

---
+++ app/src/main/java/com/RecipesDao.kt
@@ -0,0 +1,15 @@
+package com
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface RecipesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @androidx.room.Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    fun readRecipes(): kotlinx.coroutines.flow.Flow<List<RecipesEntity>>
+}
