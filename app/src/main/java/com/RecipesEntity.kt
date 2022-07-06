fcfe3ecaeb05cdd65f7fa806d164908c03d9a4bc
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-07-06 16:29:19 +0400

    entity class done

---
+++ app/src/main/java/com/RecipesEntity.kt
@@ -0,0 +1,13 @@
+package com
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.elchinasgarov.models.FoodRecipe
+
+@Entity(tableName = "recipes_table")
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
